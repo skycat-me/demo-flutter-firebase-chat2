965747cc88c850b6e0155073d2ba2d669e2c384e
大和田知洋 <owada.tomohiro@gwx.co.jp>
2019-10-27 11:26:14 +0900

    Add: comments

--- lib/blocs/chat.dart
+++ lib/blocs/chat.dart
@@ -4,6 +4,20 @@ class ChatBloc {
   final FirebaseUser user;
 
   // outputs
+  /*
+    messagesのStreamでFireStoreからリアルタイムに流れてくる
+    メッセージのsnapshotを監視します。
+    Message.fromDocumentSnapshotは流れてくるデータを整形しています。
+    model > message.dart で詳細を確認できます。
+
+    今はメッセージの削除機能などがないため問題ありませんが
+    Streamにはaddやdelete,updateなどいろんなイベントが流れてくるので
+    イベントのタイプも監視し処理を分岐する必要があります。
+
+    _postMessageはメッセージの投稿を行います。
+    FiresStoreのデータの登録はset,addやupdateなどいくつかのメソッドを
+    使い分けで新規登録、編集機能などを作成します
+   */
   final Stream<List<Message>> messages;
 
   final BehaviorSubject<String> _postMessage = BehaviorSubject<String>();
